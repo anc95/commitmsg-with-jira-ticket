9b9e93211a5599d4d1a845fc5bec8059b078e2ed
anc95 <1481988258@qq.com>
2022-01-25 11:23:45 +0800

    feat: commit and install command

--- bin/add-jira-ticket.js
+++ bin/add-jira-ticket.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
+const { program } = require('commander');
 const { getIssueTicket }  = require('../dist/index');
-const commitFile = process.argv[2]
 
-const main = async () => {
-    const ticket = await getIssueTicket();
-    const content = fs.readFileSync(commitFile);
-    fs.writeFileSync(commitFile, `${ticket} ${content}`)
+const prependTicket = async (commitmsgFile, host) => {
+    const ticket = await getIssueTicket(host);
+    const content = fs.readFileSync(commitmsgFile);
+    fs.writeFileSync(commitmsgFile, `${ticket} ${content}`)
 }
 
-if (commitFile === 'install') {
+const install = (host) => {
     const gitDir = path.join(process.cwd(), '.git');
 
     if (!fs.existsSync(gitDir)) {
@@ -23,17 +23,29 @@ if (commitFile === 'install') {
         fs.writeFileSync(prepareCommitMsgFile, '');
     }
 
-    const scriptToAppend = `\nexec < /dev/tty && npx commitmsg-with-jira-ticket`
+    const scriptToAppend = `\nexec < /dev/tty && npx commitmsg-with-jira-ticket commit $1 --host=${host}`
 
-    const commitMsgFileContent = fs.readFileSync(prepareCommitMsgFile, 'utf-8');
-
-    if (commitMsgFileContent.includes('commitmsg-with-jira-ticket')) {
-        return;
-    }
+    let commitMsgFileContent = fs.readFileSync(prepareCommitMsgFile, 'utf-8');
+    commitMsgFileContent = commitMsgFileContent.replace(/.*commitmsg-with-jira-ticket.*/g, '')
 
     fs.writeFileSync(prepareCommitMsgFile, commitMsgFileContent + scriptToAppend);
     console.info('Successfully installed!')
 }
-else {
-    main();
-}
+
+program
+    .version(require('../package.json').version)
+    .command('commit')
+    .argument('<commitmsgFile>')
+    .requiredOption('--host <host>', 'Your jira host')
+    .action(async (commitmsgFile, options) => {
+        await prependTicket(commitmsgFile, options.host);
+    })
+
+program
+    .command('install')
+    .requiredOption('--host <host>', 'Your jira host')
+    .action((options) => {
+        install(options.host);
+    })
+
+program.parse(process.argv)
